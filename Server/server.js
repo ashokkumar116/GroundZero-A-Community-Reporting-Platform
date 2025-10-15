f46eb2e091e923e3d929f07ba024b821d1fe2fc1
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-10-15 22:15:02 +0530

    Create Express Server

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.FRONTEND_APP_URL,
+    credentials:true
+}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
