b82d12a8ab02c58a45c1af21a87cc590aa0f1e4c
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-11-02 20:24:55 +0530

    Create Volunteer Request Schema

---
+++ Server/models/VolunteerRequest.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+
+const VolunteerRequestSchema = new mongoose.Schema({
+    report:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Reports",
+        required:true,
+    },
+    volunteer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    note:{
+        type:String,
+        default:"",
+    },
+    requestedAt:{
+        type:Date,
+        default:Date.now,
+    },
+    reviewedBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:null
+    },
+    reviewedAt:{
+        type:Date,
+        default:null
+    },
+    status:{
+        type:String,
+        enum:["pending","approved","rejected"],
+        default:"pending"
+    },
+},{
+    timestamps:true
+})
+
+export const VolunteerRequest = mongoose.model.VolunteerRequest || mongoose.model("VolunteerRequest",VolunteerRequestSchema);
