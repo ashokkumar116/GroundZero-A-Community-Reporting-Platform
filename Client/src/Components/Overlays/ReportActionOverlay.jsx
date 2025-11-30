047e1ca989d0a1c6d7c07bcf175296b49bdd900d
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-11-30 20:11:57 +0530

    Integrate Delete Function in Frontend

--- Client/src/Components/Overlays/ReportActionOverlay.jsx
+++ Client/src/Components/Overlays/ReportActionOverlay.jsx
@@ -1,9 +1,12 @@
-import { ConfirmPopup } from 'primereact/confirmpopup';
+import { confirmPopup, ConfirmPopup } from 'primereact/confirmpopup';
 import { OverlayPanel } from 'primereact/overlaypanel'
 import React from 'react'
 import { HiOutlineExternalLink } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom'
-import { FiEdit } from 'react-icons/fi';
+import { FiDelete, FiEdit } from 'react-icons/fi';
+import { MdDeleteOutline, MdOutlineDelete } from 'react-icons/md';
+import axios from '../../Services/axios';
+import toast from 'react-hot-toast';
 
 const ReportActionOverlay = ({panelRef,selectedReport,setModalVisible,getReports}) => {
   const navigate = useNavigate();
@@ -11,13 +14,32 @@ const ReportActionOverlay = ({panelRef,selectedReport,setModalVisible,getReports
   const reportId = selectedReport?._id;
   const handleEdit = () => {
     setModalVisible(true);
+    panelRef.current.hide();
   };
-  const handleDelete = () => {
-    
+  const handleDelete = async() => {
+    const response = await axios.delete(`/admin/deletereport/${reportId}`);
+    if(response.status === 200){
+        getReports();
+        panelRef.current.hide();
+        toast.success("Report Deleted Successfully");
+    }
   }
+
+  const removeReportConfirm = (event) => {
+          confirmPopup({
+              target: event.currentTarget,
+              message: "Are you sure you want to remove this report?",
+              icon: "pi pi-exclamation-triangle",
+              acceptLabel: "Yes",
+              rejectLabel: "No",
+              accept: () => handleDelete(),
+              reject: () => panelRef.current.hide()
+          })
+      }
+
   return (
     <OverlayPanel ref={panelRef} className='flex flex-col gap-2'>
-            <ConfirmPopup />
+            <ConfirmPopup  />
             <div className='p-2 hover:bg-gray-200 transition duration-300 ease-in-out cursor-pointer flex items-center gap-2 rounded-md' onClick={()=>{navigate(`/issues/${reportId}`)}}>
                 <HiOutlineExternalLink />
                 <p className='text-sm'>View Issue</p>
@@ -25,6 +47,10 @@ const ReportActionOverlay = ({panelRef,selectedReport,setModalVisible,getReports
             <div className='p-2 hover:bg-gray-200 transition duration-300 ease-in-out cursor-pointer flex items-center gap-2 rounded-md' onClick={handleEdit}>
                 <FiEdit/>
                 <p className='text-sm'>Edit Report</p>
+            </div>
+            <div className='p-2 hover:bg-gray-200 transition duration-300 ease-in-out cursor-pointer flex items-center gap-2 rounded-md' onClick={removeReportConfirm}>
+                <MdOutlineDelete />
+                <p className='text-sm'>Delete Report</p>
             </div>
             
         </OverlayPanel>
