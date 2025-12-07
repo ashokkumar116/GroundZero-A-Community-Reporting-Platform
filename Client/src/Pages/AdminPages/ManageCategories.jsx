53899d03795d54445ceeda48ccd7c45a126ebf3f
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-12-07 14:07:55 +0530

    Add Coming Soon UI for Future Features

--- Client/src/Pages/AdminPages/ManageCategories.jsx
+++ Client/src/Pages/AdminPages/ManageCategories.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FiClock } from 'react-icons/fi'
 
 const ManageCategories = () => {
   return (
-    <div>ManageCategories</div>
+    <div className='flex flex-col gap-5 items-center justify-center h-[calc(100vh-15rem)]'>
+
+      <FiClock className="text-5xl text-gray-600" />
+      <p className="mt-2 text-gray-700 font-medium">Coming Soon</p>
+      <p className="text-xs text-gray-500">This feature is under development</p>
+
+    </div>
   )
 }
 
